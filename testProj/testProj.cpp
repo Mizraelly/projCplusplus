49cb5f26cce04b7da0d57c32ec814812cf2e60b8
Aleks Dmitruk <aleks.dmitruk2014@gmail.com>
2020-07-16 19:41:58 +0300

    learn string concatination

--- testProj/testProj.cpp
+++ testProj/testProj.cpp
@@ -1,13 +1,13 @@
 ﻿#include <iostream>
 using namespace std;
+
 int Factorial(int N);
 void Swap(int* a, int* b);
-void showInfo(int(*foo)(int str));
+void showInfo(int(*foo)(int) , int arg);
 int getSimb(int num);
 
 int Factorial(int N) 
 {
-
     if (N == 1) return 1;
     else if (N == 0) return 1;
     return N * Factorial(N - 1);
@@ -32,16 +32,23 @@ void showInfo(int(*foo)( int ) , int arg )
 
 int main()
 {
-    int(*foo)(int) = getSimb;
+    // function pointer
+    /*int(*foo)(int) = getSimb;
+    showInfo(foo, 5);*/
+
+    //string concatination
+   /* string a, b, c;
+    a = "hello";
+    b = "world";
+    cout << a + ' ' +  b << endl;*/
     
-    
-    showInfo(foo, 5);
-    
-   /* int a = 5;
+
+    /*int a = 5;
     int b = 6;
     cout << "a = " << a << " b = " << b << endl;
     Swap(&a, &b);
     cout << "a = " << a << " b = " << b << endl;*/
+
     /*setlocale(LC_ALL, "Rus");
     int a;
     do {       
@@ -49,8 +56,8 @@ int main()
         cin >> a;
         cout << "факториал " << a << " равен:" << Factorial(a) << endl;
     } while (a != 404);*/
-    
-    
+   
+    system("pause");
 }
 
 
